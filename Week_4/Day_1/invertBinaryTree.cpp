a6714301368e48c6dd8712e1fef42e3820788665
adarshverse <adarshshree415@gmail.com>
2026-06-22 16:03:10 +0530

    Week4 Day1 Assignment

---
+++ Week_4/Day_1/invertBinaryTree.cpp
@@ -0,0 +1,33 @@
+// Invert Binary Tree
+
+#include<iostream>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    void helper(TreeNode* root) {
+        if (root == NULL) return;
+        else {
+            TreeNode* temp = root->left;
+            root->left = root->right;
+            root->right = temp;
+        }
+        helper(root->left);
+        helper(root->right);
+    }
+
+    TreeNode* invertTree(TreeNode* root) {
+        helper(root);
+        return root;
+    }
+};
